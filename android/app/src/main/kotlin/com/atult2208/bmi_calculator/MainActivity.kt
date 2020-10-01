d82072cd16bef5dbd5f7c00d7ae15b8763c67c84
= <atult2208@gmail.com>
2020-10-01 08:38:48 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/atult2208/bmi_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.atult2208.bmi_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0XFF2D3142),
+        scaffoldBackgroundColor: Color(0XFF2D3142),
+        accentColor: Color(0XFFEF8354),
+        textTheme: TextTheme(
+          body1: TextStyle(color: Colors.white),
+        )
+    ),
+      home: InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
